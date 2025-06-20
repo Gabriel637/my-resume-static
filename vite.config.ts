7b2e07031ab6060fb16f8d09385f5c595da6ced1
Gabriel Barbosa <gabriel.amorim637@gmail.com>
2025-06-20 10:57:59 -0300

    deploy to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/",
+  base: "/my-resume-static/",
   plugins: [react(), tailwindcss()],
 });
